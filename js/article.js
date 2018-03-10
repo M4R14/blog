e505d81a18a1b5437bce925fb3ff90f8c2c55f42
mark vachi <vachirawit.mark@gmail.com>
2018-03-10 23:56:05 +0700

    side nav 2

--- js/article.js
+++ js/article.js
@@ -10,22 +10,58 @@ const getQuery = (variable) => {
 }
 const EMAIL = 'vachirawit.mark@gmail.com';
 const content = getQuery('content');
-fetch(`content/${content}/info.json`)
-  .then(response => response.json())
-  .then(info => {
-    const article_tags = document.getElementById('article-tag');
-    info.tag.forEach(tag => {
-      const span = document.createElement('span');
-      span.textContent = tag;
-      span.className = "badge badge-light";
-      article_tags.appendChild(span);
+
+if(content){
+  fetch(`content/${content}/info.json`)
+    .then(response => response.json())
+    .then(info => {
+      const article_tags = document.getElementById('article-tag');
+      info.tag.forEach(tag => {
+        const a = document.createElement('a');
+        a.href = `?tag=${tag}`;
+        const span = document.createElement('span');
+        span.textContent = tag;
+        span.className = "badge badge-light";
+        a.appendChild(span);
+        article_tags.appendChild(a);
+      })
     })
-  })
-fetch(`content/${content}/index.html`)
-  .then(response => response.text())
-  .then(text => {
-    const content = document.getElementById('article');
-    content.innerHTML = text;
-    const ct_title = content.querySelector('.content-title');
-    document.title = 'M4R14 | ' + ct_title.textContent;
-  })
+  fetch(`content/${content}/index.html`)
+    .then(response => response.text())
+    .then(text => {
+      const content = document.getElementById('article');
+      content.innerHTML = text;
+      const ct_title = content.querySelector('.content-title');
+      document.title = 'M4R14 | ' + ct_title.textContent;
+    })
+}
+
+const tag = getQuery('tag');
+if(tag){
+  fetch(`content/content.json`)
+    .then(response => response.json())
+    .then(content_list => {
+      const article_tags = document.getElementById('article');
+      const ul = document.createElement('ul');
+      content_list.forEach(content => {
+        if(content.tag.find(con_tag => con_tag == tag)){
+          article_tags.appendChild(card(content))
+        }
+      })
+      article_tags.appendChild(ul);
+    })
+}
+
+const card = (info) => {
+  const div = document.createElement('div');
+  div.className = "card mb-3";
+  div.innerHTML =  `
+    <div class="card-body">
+      <h5 class="card-title">
+        <a href="?content=${info.filename}">${info.title}</a>
+      </h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>`;
+  return div;
+}
--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 [
-  { filename: 'side-nav', target: 'side-nav' },
   { filename: 'header', target: 'header' },
   { filename: 'footer', target: 'footer' },
-  { filename: 'comment', target: 'comment' },
 ].forEach(info => {
   fetch(`layout/${info.filename}.html`)
     .then(response => response.text())
@@ -11,3 +9,41 @@
       content.innerHTML = text;
     })
 })
+
+fetch(`layout/side-nav.html`)
+  .then(response => response.text())
+  .then(text => {
+    const content = document.getElementById('side-nav');
+    content.innerHTML = text;
+    fetch(`content/content.json`)
+      .then(response => response.json())
+      .then(articles => {
+        const tags_ul = content.querySelector('.tags ul')
+        const archives = content.querySelector('.archives ul')
+        let tags = [];
+        let dataTime_list = [];
+        articles.forEach(article => {
+          article.tag.forEach(atg => {
+            if(!tags.find((tag) => tag === atg)){
+              tags_ul.appendChild(createTag('tag', atg));
+              tags.push(atg);
+            }
+          })
+          const article_dataTime = moment(article.dataTime);
+          if(!dataTime_list.find((dataTime) => dataTime == article_dataTime.year())){
+            archives.appendChild(createTag('archive', article_dataTime.year()));
+            dataTime_list.push(article_dataTime.year());
+          }
+        });
+      })
+  })
+
+const createTag = (qury, tagName) => {
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  a.href = `?${qury}=${tagName}`;
+  a.textContent = tagName;
+  li.appendChild(a);
+
+  return li;
+}
